35e64e8ac347fbf45f1f802a600c16561207c595
ezekielkouassi <121268728+ezekielKouassi-dev@users.noreply.github.com>
2022-12-27 21:01:43 +0000

    first commit : feat array work

---
+++ script.js
@@ -0,0 +1,21 @@
+// Exercice 1
+
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.shift();
+
+fruits.sort();
+
+fruits.push("Kiwi");
+
+fruits.splice(0, 1);
+
+fruits.reverse();
+
+console.log(fruits);
+
+// Exercice 2
+
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(moreFruits[1][1][0]);
